dea94dc4a8704bfb4bce8a45f3abce7b9e9bccae
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-13 23:31:03 +0530

    add files

---
+++ forthapp/models/person.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+const PersonSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    name: {
+        type: String,
+        require: true
+    },
+
+    name: {
+        type: String,
+        require: true
+    },
+})
